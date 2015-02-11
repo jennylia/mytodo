410a5678747f1d469d28ad5ad921438e02d6c85b
jennylia <jennylian100@gmail.com>
2015-02-10 20:39:13 -0800

    so just doing some auto generated stuff with bower

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,5 +14,22 @@ angular.module('mytodoApp')
       'AngularJS',
       'Karma'
     ];
+    function checkDux(item)
+    {
+        for (var i = 0; i< $scope.todos.length; i++)
+        { if($scope.todos[i] == item)
+            return false; 
+        }
+        return true;
+    }
     $scope.todos=['Item 1', 'Item 2', 'Item 3'];
+    $scope.addTodo = function(){
+        if(checkDux($scope.todo))
+            $scope.todos.push($scope.todo);
+        $scope.todo = '';
+    };
+
+    $scope.removeTodo = function(index){
+        $scope.todos.splice(index,1);
+    }
   });
