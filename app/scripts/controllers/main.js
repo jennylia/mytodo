5648fcdbcf5a4e850bcdd9774853ff7426fcec40
jennylia <jennylian100@gmail.com>
2015-02-14 00:18:13 -0800

    try to do the database for heroku

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,38 +1,23 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name mytodoApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the mytodoApp
- */
 angular.module('mytodoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    function checkDux(item)
-    {
-        for (var i = 0; i< $scope.todos.length; i++)
-        { if($scope.todos[i] === item)
-            {
-            return false; 
-            }
-         } return true;
-    }
-    $scope.todos=['Item 1', 'Item 2', 'Item 3'];
-    $scope.addTodo = function(){
-        if(checkDux($scope.todo))
-        {
-            $scope.todos.push($scope.todo);
-         }
-        $scope.todo = '';
-    };
+  .controller('MainCtrl', function ($scope, localStorageService) {
 
-    $scope.removeTodo = function(index){
-        $scope.todos.splice(index,1);
-    };
-  });
+              var todosInStore = localStorageService.get('todos');
+
+                  $scope.todos = todosInStore || [];
+
+                      $scope.$watch('todos', function () {
+                                localStorageService.set('todos', $scope.todos);
+                                    }, true);
+
+                          $scope.addTodo = function () {
+                                $scope.todos.push($scope.todo);
+                                      $scope.todo = '';
+                                          };
+
+                                              $scope.removeTodo = function (index) {
+                                                    $scope.todos.splice(index, 1);
+                                                        };
+
+                                                          });
